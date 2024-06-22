1dbe52821d637e89ce169ca44a4f87562be3fb90
b2bthab <79100161+b2bthab@users.noreply.github.com>
2024-06-22 17:13:52 +0000

    20.06.2024 nodarbiba

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/services')
-def index():
-    return render_template('services.html') #comment
+def services():
+    return render_template('services.html')
+
+@app.route('/Portfolio')
+def Portfolio():
+    return render_template('Portfolio.html')
+
+@app.route('/par_mums_2')
+def par_mums():
+    return render_template('par_mums_2.html')
+
+@app.route('/kontakti')
+def kontakti():
+    return render_template('kontakti.html')
+
+@app.route('/aiziet', methods=['POST'])
+def aiziet():
+    lietotajs = request.form['lietotajvards']
+    return f"Paldies, {lietotajs}! Jūsu ziņa ir saņemta"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
-
-
